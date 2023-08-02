f495c003d6c94bed4067a167463285af787f1870
Jin Hyuk <ekrrdj07@gmail.com>
2023-08-03 04:11:30 +0900

    6주차 과제 완료

--- src/test/java/com/example/kakao/order/OrderRestControllerTest.java
+++ src/test/java/com/example/kakao/order/OrderRestControllerTest.java
@@ -1,90 +1,89 @@
-package com.example.kakao.product;
+package com.example.kakao.order;
 
 import com.example.kakao.MyRestDoc;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @AutoConfigureRestDocs(uriScheme = "http", uriHost = "localhost", uriPort = 8080)
 @ActiveProfiles("test")
-@Sql("classpath:db/teardown.sql")
+@Sql(value = "classpath:db/teardown.sql")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-public class ProductRestControllerTest extends MyRestDoc {
-
+public class OrderRestControllerTest extends MyRestDoc {
+    @Autowired
+    private ObjectMapper om;
+    @WithUserDetails(value = "ssarmango@nate.com")
     @Test
-    public void findAll_test() throws Exception {
-        // given teardown.sql
-
-        // when
-        ResultActions resultActions = mvc.perform(
-                get("/products")
-        );
-
-        // console
-        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
-        System.out.println("테스트 : "+responseBody);
-
-        // verify
-        resultActions.andExpect(jsonPath("$.success").value("true"));
-        resultActions.andExpect(jsonPath("$.response[0].id").value(1));
-        resultActions.andExpect(jsonPath("$.response[0].productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
-        resultActions.andExpect(jsonPath("$.response[0].description").value(""));
-        resultActions.andExpect(jsonPath("$.response[0].image").value("/images/1.jpg"));
-        resultActions.andExpect(jsonPath("$.response[0].price").value(1000));
-        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
-    }
-
-    @Test
-    public void findById_test() throws Exception {
-        // given teardown.sql
-        int id = 1;
-
-        // when
-        ResultActions resultActions = mvc.perform(
-                get("/products/" + id)
-        );
-
-        // console
-        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
-        System.out.println("테스트 : "+responseBody);
-
-        // verify
-        resultActions.andExpect(jsonPath("$.success").value("true"));
-        resultActions.andExpect(jsonPath("$.response.id").value(1));
-        resultActions.andExpect(jsonPath("$.response.productName").value("기본에 슬라이딩 지퍼백 크리스마스/플라워에디션 에디션 외 주방용품 특가전"));
-        resultActions.andExpect(jsonPath("$.response.description").value(""));
-        resultActions.andExpect(jsonPath("$.response.image").value("/images/1.jpg"));
-        resultActions.andExpect(jsonPath("$.response.price").value(1000));
-        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
-    }
-    @DisplayName("DB 상품에 등록되지않는 productId를 요청받았을 때 예외")
-    @Test
-    public void findById_fail_test() throws Exception {
+    public void save_test() throws Exception{
         //given
-        int id = 16; //DB 상품에 등록되지않는 productId를 요청받았을 때
+
 
         //when
         ResultActions resultActions = mvc.perform(
-                get("/products/" + id)
+                post("/orders/save")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
         );
 
-        //console
         String responseBody = resultActions.andReturn().getResponse().getContentAsString();
         System.out.println("테스트 : " + responseBody);
 
-        //verify
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("true"));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void findById_test() throws Exception {
+        //given
+        int userId = 1;
+        //when
+        ResultActions resultActions = mvc.perform(
+                get("/orders/" + userId)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
+        resultActions.andExpect(jsonPath("$.success").value("true"));
+        resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
+    }
+    @DisplayName("DB Item에 요청으로 온 orderId에 해당하는 item이 없는 경우 예외")
+    @WithUserDetails(value = "ssarmango@nate.com")
+    @Test
+    public void findById_fail_test() throws Exception {
+        //given
+        int userId = 2;
+        //when
+        ResultActions resultActions = mvc.perform(
+                get("/orders/" + userId)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        );
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // verify
         resultActions.andExpect(jsonPath("$.success").value("false"));
-        resultActions.andExpect(jsonPath("$.error.message").value("해당 상품을 찾을 수 없습니다 : "+id));
+        resultActions.andExpect(jsonPath("$.error.message").value("item 리스트에 null이 들어갔거나 orderId에 해당하는 item을 찾을 수 없습니다"));
         resultActions.andDo(MockMvcResultHandlers.print()).andDo(document);
     }
 }
